f616d959033d4c8ebef33f2452e05cf2dc8d90f9
dbyrd <methyl8@comcast.net>
2022-10-13 15:06:40 -0500

    browse routes

--- controllers/browseRoutes.js
+++ controllers/browseRoutes.js
@@ -1,26 +1,30 @@
 const { Movie } = require('../models');
+const { Op } = require("sequelize");
 
 const router = require('express').Router();
 // route /browse
 router.get('/', async (req, res) => {
-    //get random items  TABLESAMPLE()?
-    let randId = Math.floor(Math.random() * 100) 
-    console.log(randId);
-    const browse = await Movie.findOne({where: {id: randId}})
-    console.log(browse);
-
-    res.send("home route")
+    //get 10 random items
+    const resultArr = [];
+    for(let i = 0; i < 10; i++) {
+        let randId = Math.floor(Math.random() * 100) 
+        const browse = await Movie.findOne({where: {id: randId}})
+        resultArr.push(browse.get({plain:true}))
+    }
+    // res.status(200).render('results', resultArr)
+    res.status(200).json(resultArr)
 });
 
-// router.get('/:genre', async (req, res) => {
-//     const genreResults = await Movie.findAll({
-//         where: {Genre: {op.like}}
-//     })
-// })
+router.get('/:genre', async (req, res) => {
+    const genreResultsDB = await Movie.findAll({
+        where: {Genre: {[Op.like]: `%${req.params.genre}%`}}
+    });
+    const genreResults = genreResultsDB.map((result) => result.get({plain:true}))
+    // res.status(200).render('results', genreResults.get({plain:true}))
+    res.status(200).json(genreResults)
+});
 
-//get /browse - random movies for collection results.handlebbars
 //get search for movie by title with post
-//get /browse/:genre
 
 
 module.exports = router;
