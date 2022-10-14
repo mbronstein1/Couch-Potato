6174d3d7f4037b8cc2d5b85851420927d61d9470
Jared <jjohns3@tulane.edu>
2022-10-14 13:23:48 -0500

    added to the modal.js page and completedthe modal on results.handlebars

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,3 +1,38 @@
-UIkit.modal(element).hide();
+const movieContainerEl = $('#movie-conatiner');
+const modalEl = $('#movie-modal')
+const modalTitleEl = $('#modal-title')
+
+UIkit.modal(modalEl).hide();
+
+movieContainerEl.on('click', 'uk-card', function (e) {
+    movieContainerEl.html("");
+    UIkit.modal(modalEl).show();
+
+    var target = e.target;
+
+    const movieTitle = $(target).attr('data-title')
+    modalTitleEl.text(movieTitle)
 
 
+
+    // if ($(target).attr('data-img-url')) {
+    //     var images = $(target).attr('data-img-url');
+    //     var imageStore = $('<img>');
+    //     imageStore.attr('src', images);
+    //     $('#modalContent').append(imageStore);
+    //   } else {
+    //     var noImageText = $('<p>');
+    //     noImageText.text("Sorry! This artwork has no image to display!")
+    //     $('#modalContent').append(noImageText);
+    //   }
+    
+    //   var modalUrl = $(target).attr('data-art-url');
+    //   var modalUrlEl = $('<a>');
+    //   modalUrlEl.attr('href', modalUrl);
+    //   modalUrlEl.attr('target', '_blank');
+    //   modalUrlEl.text("View more info here!");
+    
+    //   $('#modalContent').append(modalUrlEl)
+    //   var modalHeader = $(target).attr('data-artworkName');
+    //   $('#modalHeader').text(modalHeader);
+})
