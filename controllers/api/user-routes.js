4f7776c044526108f7f6454151e14038fea58361
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-10-14 11:38:52 -0500

    added favorite post route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,91 +1,93 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Favorite } = require('../../models');
 
 // CREATE new user
 router.post('/', async (req, res) => {
-    try {
-      const dbUserData = await User.create(req.body);
-  
-      req.session.save(() => {
-        req.session.loggedIn = true;
-        req.session.user_id = dbUserData.id;
-        res.status(200).json(dbUserData);
+  try {
+    const dbUserData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.user_id = dbUserData.id;
+      res.status(200).json(dbUserData);
     });
 
-} catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // Login
 router.post('/login', async (req, res) => {
-    try {
-      const dbUserData = await User.findOne({
-        where: {
-          email: req.body.email,
-        },
-      });
-  
-      if (!dbUserData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email. Please try again!' });
-        return;
-      }
-  
-      const validPassword = await dbUserData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password. Please try again!' });
-        return;
-      }
-  
-      req.session.save(() => {
-        req.session.loggedIn = true;
-        req.session.user_id = dbUserData.id;
-        console.log(
-          '🚀 ~ file: user-routes.js ~ line 57 ~ req.session.save ~ req.session.cookie',
-          req.session.cookie
-        );
-  
-        res
-          .status(200)
-          .json({ user: dbUserData, message: 'You are now logged in!' });
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
 
-  // Logout
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email. Please try again!' });
+      return;
+    }
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.user_id = dbUserData.id;
+      console.log(
+        '🚀 ~ file: user-routes.js ~ line 57 ~ req.session.save ~ req.session.cookie',
+        req.session.cookie
+      );
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Logout
 router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end();
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.post('/collection', async (req, res) => {
+  try {
+    if (!req.session.loggedIn) {
+      res.render('login');
+    }
+    else {
+      const addFavorite = await Favorite.create({
+        user_id: req.session.user_id,
+        movie_id: req.body.movie_id,
       });
-    } else {
-      res.status(404).end();
+      res.status(200).json(addFavorite);
     }
-  });
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-  router.post('/collection', async (req, res) => {
-    try{
-      if(!req.session.loggedIn) {
-        res.render('login');    
-      }
-      else {
-        const addFavorite = Favorite.create(req.body, {
-  
-        })     
-      }
-    }
-    catch(err) {
-      res.status(400).json(err);
-    }
-  })
-
-  module.exports = router;
+module.exports = router;
