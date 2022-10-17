4c390d9d61256a17fc8980c3b18bca1558eda2cf
Matthew Bronstein <mbronstein1@gmail.com>
2022-10-17 11:24:25 -0500

    added comments and cleaned up search-aside, home, login, and results handlebars, and login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,10 @@
+// Login form logic
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
-
-  console.log(email);
-  console.log(password);
 
   if (email && password) {
     // Send a POST request to the API endpoint
@@ -25,16 +23,16 @@ const loginFormHandler = async (event) => {
   }
 };
 
+// Signup form logic
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
+  // Collect values from signup form
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
-
-  console.log(email);
-  console.log(password);
   
   if (email && password) {
+    // Send a post request to the API endpoint
     const response = await fetch('/api/users', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
@@ -42,6 +40,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
+      // If successful, redirect the browser to the profile page
       document.location.replace('/');
     } else {
       alert(response.statusText);
@@ -49,10 +48,12 @@ const signupFormHandler = async (event) => {
   }
 };
 
+// Event listener on login form submit
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
 
+// Event listener on signup form submit
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
